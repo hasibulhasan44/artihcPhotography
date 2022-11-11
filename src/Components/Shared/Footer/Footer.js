c9a3fd713dbac4e0dc5ef1147a95330996ec2b84
hasibulhasan44 <hasanhasib7367@gmail.com>
2022-11-11 14:39:31 +0600

    footer section done

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -7,7 +7,9 @@ const Footer = () => {
         <div className='bg-gray-300 mt-8 rounded-lg'>
             <div className='flex  items-center'>
                 <img className='w-1/4 mr-4 mt-8' src={img} alt="" />
-                
+                <div className='text-black ml-2'>
+                    <h1>ARTIHC is one of the most exciting platform for clients who wants their moment to be memoriable with a fine CLICK. We fulfill your wishes passionately. Customer is like our own family member to us. We can gurantee that you will not find more convenient service than ours anywhere. So make your moments memoriable with us! We Welcome you.</h1>
+                </div>
                 <div className='mr-4'>
                     <h2 className="text-md mb-4 text-center">Follow us On</h2>
                     <div className='flex justify-evenly'>
